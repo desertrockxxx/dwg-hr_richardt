217942e0a1e5211b512a71fa79d000c2d85469b1
Sinan Karateke <s.karateke@gmx.de>
2017-03-20 13:10:56 +0000

    Ich hab keine Ahnung was ich mache

--- js/oldschool.js
+++ js/oldschool.js
@@ -1,23 +1,25 @@
 function checkField(){
     // Teste ob Feld angezeigt wird
-    var checkFormClass = checkFormClass()
-    alert(checkFormClass);  
+    var checkFormClass1 = checkFormClass()
+
+    if(checkFormClass1){
+        document.getElementsByClassName("search-form")[0].className = "search-form"
+    } else{
+        document.getElementsByClassName("search-form")[0].className = "hide search-form"
+    }
     // Zugriff auf Feld (Wert aus Feld)
-   
     // Prüfen ob Feldwert eingetragen
-    
     // Abschicken des Formulars
 }
 
 function checkFormClass(){
     // Greife auf das Element Form zu und schaue ob Klasse Field besetzt ist
     // Info: [0] es ist das null'te Formular also das Erste
-    var formClass = document.getElementsByClassName("search-form")[0].className;
-    
+    var formClass = document.getElementsByClassName("search-form")[0].className
+
     // Wenn ja check = true
-    if(formClass == "hide") {
+    if(formClass.indexOf("hide") >= 0) {
         check = true;
-    
     // Ansonsten check = false   
     } else {
         check = false;
