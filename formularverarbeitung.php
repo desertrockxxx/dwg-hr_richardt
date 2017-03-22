95b0c3e509a363d54819fe673453dea416debe13
Sinan Karateke <s.karateke@gmx.de>
2017-03-22 09:31:37 +0000

    JS verändert

---
+++ formularverarbeitung.php
@@ -0,0 +1,46 @@
+<?php 
+function gibAlter($datum){
+//dd.mm.YYY
+$datumsArray = explode(".", $datum);
+// array(15,03,1992);
+// 1992-03-15
+$jetzt = mktime(0,0,0, date('m'), date('d'), date('Y'));
+$geburtstag = mktime(0,0,0, $datumsArray[1], $datumsArray[0], $datumsArray[2]);
+$alter = intval(($jetzt - $geburtstag) / (3600 * 24 * 365));
+return $alter;
+};
+
+?>
+
+<!DOCTYPE html>
+<head>
+	<meta charset="UTF-8">
+	<title>Formulare</title>
+	<script src="js/main.js"></script>
+</head>
+<body>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
+	<div>
+		<input type="text" name="vorname" placeholder="Vorname">
+	</div>
+	<div>
+		<input type="text" name="nachname" placeholder="Nachname">
+	</div>
+	<div>
+		<input onkeyup="ueberpruefe(this);" type="text" name="geburt" placeholder="Geburtsdatum (dd.mm.YYYY)">
+	</div>
+	<input type="submit" name="absenden" value="Senden">
+</form>
+<?php if(isset($_GET['absenden'])): ?>
+	<p> Hallo 
+	<?php echo $_GET['vorname'] . " " . $_GET['nachname']; ?>
+	</p>
+	<?php if(!empty($_GET['geburt'])): ?>
+		<p>Ihr Alter beträgt: <?php echo gibAlter($_GET['geburt']); ?></p>
+	<?php endif; ?>
+<?php endif; ?>
+<?php 
+// Inhalt aus Formularfeld verarbeiten (Umwandlung in Datumsformat womit berechnet werden kann
+// Berechnen des Alters der Person (mit Ausgabe des aktuellen Alters)
+?>
+</body>
--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-$(document).ready(function(){
-	// wenn auf glypicon-search (das innerhalb des .'s' ist) geklickt
-	$(".glyphicon-search").click(function()
-	{	
-	    // wenn formular ausgefüllt submit = mit val() testen
-		if($("form .search-field").val() !="")
-		{
-			// schicke es ab
-			$(".search-form").submit();
-		}else{
-			// ansonsten schließen
-			$(".search-form").toggleClass("hide");
-		}
-	});
-});
+function ueberpruefe(elem){
+	var inhalt = elem.value;
+	var aktuellesZeichen = inhalt.charAt(inhalt.length - 1);
+	alert(aktuellesZeichen);
+	if(isNaN(aktuellesZeichen)){
+		elem.value = "";
+	}
+	//alert(inhalt.length);
+	if(inhalt.length == 2 || inhalt.length == 5){
+		elem.value = elem.value + ".";
+	}
+	
+}
