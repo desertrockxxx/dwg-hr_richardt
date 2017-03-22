2fdf73c9dbac9dc4098c03e43954fce8c64adb81
Sinan Karateke <s.karateke@gmx.de>
2017-03-22 07:00:57 +0000

    Ein paar Änderungen

--- js/oldschool.js
+++ js/oldschool.js
@@ -1,34 +1,48 @@
+var loop = 1;
 function checkField(){
-    // Teste ob Feld angezeigt wird
-    
-    var checkFormClass1 = checkFormClass()
-    var searchField = document.getElementsByClassName("search-field")[0]
-    var searchForm = document.getElementsByClassName("search-form")[0]
+	//Teste ob Feld angezeigt wird oder nicht
+	var checkFormClass1 = checkFormClass();
+	var searchfield = document.getElementsByClassName("search-field")[0];
+	var searchform = document.getElementsByClassName("search-form")[0];
+	//console.log(searchfield.value.length);
+	
+	if(searchfield.value != "" && searchfield.value.length >= 3){
+		searchform.submit();
+	} 
+	
+	if( searchfield.value.length < 3 && !checkFormClass1 && loop == 1){
+		//alert("Bitte mind. 3 Zeichen eingeben.");
+		document.getElementsByClassName("search-list")[0].innerHTML = "Mind. 3 Zeichen Eingeben " + document.getElementsByClassName("search-list")[0].innerHTML;
+		setTimeout(function(){
+			document.getElementsByClassName("search-list")[0].innerHTML =	document.getElementsByClassName("search-list")[0].innerHTML.replace("Mind. 3 Zeichen Eingeben", "");
+			loop = 1;
+		}, 5000)
+		
+		loop++;
+	}
+	
+	if(checkFormClass1){
+		document.getElementsByClassName("search-form")[0].className = "search-form";
+	} else {
+		document.getElementsByClassName("search-form")[0].className = "hide search-form";
+	}	
+	// Zugriff auf Feld (Wert aus Feld)
 
-    if(searchField.value != "") {
-        searchForm.submit()
-    }
-    
-    if(checkFormClass1){
-        document.getElementsByClassName("search-form")[0].className = "search-form"
-    } else {
-        document.getElementsByClassName("search-form")[0].classname = "hide search-form"
-    }
-    // Zugriff auf Feld (Wert aus Feld) 
 }
 
 function checkFormClass(){
-    // Greife auf das Element Form zu und schaue ob Klasse Field besetzt ist
-    // Info: [0] es ist das null'te Formular also das Erste
-    var formClass = document.getElementsByClassName("search-form")[0].className
-
-    // Wenn ja check = true
-    if(formClass.indexOf("hide") >= 0) {
-        check = true;
-    // Ansonsten check = false   
-    } else {
-        check = false;
-    }
+	//Greife auf das Element Form zu und schaue ob Klasse hide gesetzt ist
+	//Wenn ja dann check = true
+	var formClass = document.getElementsByClassName("search-form")[0].className;
+	
+	if(formClass.indexOf("hide") >= 0){
+		check = true;
+	} else{
+		check = false;
+	}
+	//ansonsten check = false	
 return check;
 }
+
+
 
